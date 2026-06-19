5ce8b8c37d32d2b2725f2af0fb54e5f4aa0d7561
hs0319yu-cmd <hs.0319yu@gmail.com>
2026-06-19 13:24:59 +0900

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,10 +1,72 @@
+const CACHE_NAME = "stock-v1";
+
+const urlsToCache = [
+
+  "/",
+
+  "/index.html",
+
+  "/manifest.json"
+
+];
+
+
+// インストール
+self.addEventListener(
+
+  "install",
+
+  event=>{
+
+    event.waitUntil(
+
+      caches.open(CACHE_NAME)
+
+      .then(cache=>{
+
+        return cache.addAll(
+
+          urlsToCache
+
+        );
+
+      })
+
+    );
+
+  }
+
+);
+
+
+// 読み込み
 self.addEventListener(
 
   "fetch",
 
-  event => {
+  event=>{
 
-    // 何もしない
+    event.respondWith(
+
+      caches.match(
+
+        event.request
+
+      )
+
+      .then(response=>{
+
+        return (
+
+          response ||
+
+          fetch(event.request)
+
+        );
+
+      })
+
+    );
 
   }
 
